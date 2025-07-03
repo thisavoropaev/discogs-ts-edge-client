c3f5cb09e65b5c0bbdc6c59d171b8c7f12333b56
thisavoropaev <thisavoropaev@gmail.com>
2025-07-03 13:51:48 +0300

    Remove unused VERSION variable

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,3 @@
 export { createDiscogsClient } from "./discogs-client.ts";
 export type { DiscogsClient, DiscogsClientConfig } from "./types/client.ts";
 export type * from "./types/endpoints/mod.ts";
-
-export const VERSION = "0.1.1";
