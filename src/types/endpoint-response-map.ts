9db94bd3a6f85e534e3254d641f118e449e100db
thisavoropaev <thisavoropaev@gmail.com>
2025-07-03 14:58:34 +0300

    Add types mapping for most of endpoints

--- src/types/endpoint-response-map.ts
+++ src/types/endpoint-response-map.ts
@@ -1,7 +1,18 @@
 import type {
+  GetArtistReleasesResponse,
+  GetArtistResponse,
+  GetLabelReleasesResponse,
+  GetLabelResponse,
+  GetMasterReleaseResponse,
+  GetReleaseRatingByUserResponse,
   GetReleaseResponse,
+  GetSearchResponse,
+  GetUserCollectionFoldersResponse,
+  GetUserCollectionItemsResponse,
   GetUserIdentityResponse,
   GetUserProfileResponse,
+  GetUserWantlistResponse,
+  UpdateReleaseRatingByUserResponse,
 } from "./endpoints/mod.ts";
 
 export type EndpointResponseMap = {
@@ -9,5 +20,21 @@ export type EndpointResponseMap = {
     "/oauth/identity": GetUserIdentityResponse;
     "/users/:username": GetUserProfileResponse;
     "/releases/:release_id": GetReleaseResponse;
+    "/artists/:artist_id": GetArtistResponse;
+    "/artists/:artist_id/releases": GetArtistReleasesResponse;
+    "/labels/:label_id": GetLabelResponse;
+    "/labels/:label_id/releases": GetLabelReleasesResponse;
+    "/masters/:master_id": GetMasterReleaseResponse;
+    "/releases/:release_id/rating/:username": GetReleaseRatingByUserResponse;
+    "/database/search": GetSearchResponse;
+    "/users/:username/collection/folders": GetUserCollectionFoldersResponse;
+    "/users/:username/collection/folders/:folder_id/releases": GetUserCollectionItemsResponse;
+    "/users/:username/wantlist": GetUserWantlistResponse;
+  };
+  PUT: {
+    "/releases/:release_id/rating/:username": UpdateReleaseRatingByUserResponse;
+  };
+  DELETE: {
+    "/releases/:release_id/rating/:username": UpdateReleaseRatingByUserResponse;
   };
 };
